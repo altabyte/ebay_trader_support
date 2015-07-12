222200a71c0427fc3d47726162c03a86fefad9ed
Rob Graham <altabyte@gmail.com>
2015-07-12 16:29:15 +0100

    Add GetSellerEvents class.

---
+++ lib/ebay_trading_pack/get_seller_events.rb
@@ -0,0 +1,99 @@
+require 'active_support/time'
+
+require 'ebay_trading'
+require 'ebay_trading/request'
+
+require_relative 'helpers/item_details'
+require_relative 'helpers/pagination'
+
+include EbayTrading
+
+module EbayTradingPack
+  class GetSellerEvents < Request
+    include Pagination, Enumerable
+
+    CALL_NAME = 'GetSellerEvents'
+
+    # @return [Symbol]
+    # Get the events type, which will be one of +:modified+, +:started+ or +:ended+
+    attr_reader :event_type
+
+    # @return [Time]
+    # The *earliest* time of interest.
+    attr_reader :time_from
+
+    # @return [Time]
+    # The *latest* time of interest.
+    attr_reader :time_to
+
+    # @return [Array<EventItem>]
+    # An array of {EventItem}s
+    attr_reader :items
+
+
+    # Get the seller events for listings that were either *started*, *ended* or *modified*
+    # within the specified time range.
+    # @param auth_token [String] the eBay Auth Token for the user submitting this request.
+    # @param [Symbol] event_type +:modified+, +:started+ or +:ended+
+    # @param [Time] time_from the earliest Time of interest.
+    # @param [Time] time_to the latest Time of interest.
+    #
+    def initialize(auth_token, event_type, time_from, time_to, args = {})
+      @event_type = event_type
+      raise RequestError.new("Event type '#{event_type}' is not valid") unless [:modified, :started, :ended].include?(event_type)
+      @time_from = time_from.utc
+      raise RequestError('Time from is not valid') unless @time_from.is_a?(Time)
+      @time_to = time_to.utc
+      raise RequestError('Time to is not valid') unless @time_to.is_a?(Time)
+
+      known_arrays = args[:known_arrays] || []
+      known_arrays.concat [:item]
+      args[:known_arrays] = known_arrays
+
+      super(CALL_NAME, auth_token, args) do
+        case event_type
+          when :modified
+            ModTimeFrom   time_from.iso8601(3)
+            ModTimeTo     time_to.iso8601(3)
+          when :started
+            StartTimeFrom time_from.iso8601(3)
+            StartTimeTo   time_to.iso8601(3)
+          when :ended
+            EndTimeFrom   time_from.iso8601(3)
+            EndTimeTo     time_to.iso8601(3)
+        end
+        IncludeWatchCount 'true'
+        DetailLevel 'ReturnAll'
+      end
+
+      @items = []
+      items = response_hash.deep_find([:item_array, :item])
+      items = [items] unless items.is_a?(Array)
+      items.each do |item_hash|
+        @items << EventItem.new(item_hash)
+      end
+    end
+
+    # Method required to be Enumerable
+    def each(&block)
+      items.each(&block)
+    end
+
+    #=======================================================================
+    class EventItem
+      include ItemDetails
+
+      attr_reader :item_hash
+
+      def initialize(item_hash)
+        @item_hash = item_hash
+      end
+
+      # Get a hash of the item details.
+      # This method is required for the Item mixin module.
+      def to_hash
+        @item_hash
+      end
+    end
+  end
+end
---
+++ spec/ebay_trading_pack/get_seller_events_spec.rb
@@ -0,0 +1,53 @@
+require 'ebay_trading'
+require 'ebay_trading_pack/get_seller_events'
+
+include EbayTrading
+include EbayTradingPack
+
+describe GetSellerEvents do
+
+  before :all do
+    EbayTrading.configure do |config|
+      config.environment  = :production
+      config.price_type   = :money
+      config.ebay_site_id = 3 # ebay.co.uk
+      config.dev_id  = ENV['EBAY_API_DEV_ID']
+      config.app_id  = ENV['EBAY_API_APP_ID']
+      config.cert_id = ENV['EBAY_API_CERT_ID']
+    end
+    @auth_token = ENV['EBAY_API_AUTH_TOKEN_TT']
+  end
+  let(:auth_token) { @auth_token }
+
+  context 'when listings ended in the last 24 hours' do
+
+    before :all do
+      @time_to = Time.now
+      @time_from = @time_to - 24.hours
+      @event_type = :ended
+
+      puts "\nGetting seller events for listings '#{@event_type.to_s}' between #{@time_from} and #{@time_to}\n\n"
+      @events = GetSellerEvents.new(@auth_token, @event_type, @time_from, @time_to, xml_tab_width: 2)
+    end
+
+    subject(:events) { @events }
+
+    it { is_expected.not_to be nil }
+    it { is_expected.to be_success }
+    it { expect(events.event_type).to be @event_type }
+    it { expect(events.items).to be_a(Array) }
+
+    it 'should provide an array of items' do
+      expect(events.items.size).to be >= 0
+      puts "Found #{events.items.size} #{@event_type.to_s} items:"
+      events.items.each { |item|
+        puts "\n#{item.summary}\n"
+      }
+    end
+
+    it 'should return something' do
+      puts JSON.pretty_generate events.response_hash
+    end
+
+  end
+end
